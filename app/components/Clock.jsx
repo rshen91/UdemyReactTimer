2560a240aa3c37c6a361dac7a03fbe287d92e9ef
Rachel <rachelwshen@gmail.com>
2017-06-27 18:15:09 -0700

    countdown component styled

--- app/components/Clock.jsx
+++ app/components/Clock.jsx
@@ -1,6 +1,12 @@
 var React = require('react');
 
 var Clock = React.createClass({
+  getDefaultProps: function () {
+    totalSeconds: 0
+  },
+  propTypes: {
+    totalSeconds: React.PropTypes.number
+  },
   formatSeconds: function(totalSeconds) {
     var seconds = totalSeconds % 60;
     var minutes = Math.floor(totalSeconds / 60);
@@ -11,13 +17,16 @@ var Clock = React.createClass({
     if (minutes < 10) {
       minutes = '0' + minutes;
     }
-    
+
     return minutes + ':' + seconds;
   },
   render: function() {
+    var {totalSeconds} = this.props;
     return (
-      <div>
-
+      <div className="clock">
+        <span className="clock-text">
+          {this.formatSeconds(totalSeconds)}
+        </span>
       </div>
     );
   }
